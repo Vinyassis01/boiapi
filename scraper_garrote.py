a2df54108c17197ddf5fcc4c0d3ddd0e2bab5eff
Vinyassis01 <vinyciusassisfonseca@gmail.com>
2025-12-23 12:15:13 -0300

    corrigindo os nomes dos arquivos

--- scraper_garrote.py
+++ scraper_garrote.py
@@ -99,38 +99,38 @@ def femea_nelore (soap):
     date = datetime.date.today()
     abertura = '{'
     fechamento = '}'
-#    with open(f'vaca boiadeira @ {datetime.date.today()}','w')as json:
-#        for vaca_boiadeira in femea_nelorada:
-#            estado = vaca_boiadeira.get_text()[1:4]
-#            valor = int(vaca_boiadeira.get_text()[6:11])
-#            arroba = valor/11
-#            json.write(f"""{abertura}
-#"date":"{date}",
-#"animal":"vaca boideira",
-#"arroba":"{arroba:.2f}",
-#"estado":"{estado}",
-#"regiao":"{estado}"
-#{fechamento}
-#,""")
-#    json.close()
+    with open(f'vaca boiadeira @ {datetime.date.today()}','w')as json:
+        for vaca_boiadeira in femea_nelorada:
+            estado = vaca_boiadeira.get_text()[1:4]
+            valor = int(vaca_boiadeira.get_text()[6:11])
+            arroba = valor/11
+            json.write(f"""{abertura}
+"date":"{date}",
+"animal":"vaca boideira",
+"arroba":"{arroba:.2f}",
+"estado":"{estado}",
+"regiao":"{estado}"
+{fechamento}
+,""")
+    json.close()
 
-#    with open(f'novilha @ {datetime.date.today()}','w')as json:
-#        for novilha in femea_nelorada:
-#            estado = novilha.get_text()[1:4]
-#            valor = novilha.get_text()[31:37]
-#            valores = float(valor.replace(',','.'))
-#            arroba = valores/9
-#            json.write(f"""{abertura}
-#"date":"{date}",
-#"animal":"novilha",
-#"arroba":"{arroba:.2f}",
-#"estado":"{estado}",
-#"regiao":"{estado}"
-#{fechamento}
-#,""")
-#    json.close()
+    with open(f'novilha @ {datetime.date.today()}','w')as json:
+        for novilha in femea_nelorada:
+            estado = novilha.get_text()[1:4]
+            valor = novilha.get_text()[31:37]
+            valores = float(valor.replace(',','.'))
+            arroba = valores/9
+            json.write(f"""{abertura}
+"date":"{date}",
+"animal":"novilha",
+"arroba":"{arroba:.2f}",
+"estado":"{estado}",
+"regiao":"{estado}"
+{fechamento}
+,""")
+    json.close()
 
-    with open(f'bezerra 7@ {datetime.date.today()}','w')as json:
+    with open(f'bezerra 7@ {datetime.date.today()}.json','w')as json:
         for bezerra in femea_nelorada:
             estado = bezerra.get_text()[1:4]
             valor = bezerra.get_text()[55:63]
